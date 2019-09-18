129c9259131ef42d1cc9588970528a8943f1dc1a
CHuks Festus <cutogu@gmail.com>
2019-09-18 17:02:50 +0100

    added styled-components

--- src/index.js
+++ src/index.js
@@ -1,9 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createGlobalStyle } from 'styled-components';
+
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const GlobalStyle = createGlobalStyle`
+  * {
+    padding: 0;
+    margin: 0;
+  }
+  body {
+    font-family: 'Poppins', sans-serif;
+  }
+`;
+
+ReactDOM.render(
+  <React.Fragment>
+    <App />
+    <GlobalStyle />
+  </React.Fragment>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
